9869572d4058b2c501f9e5b0d81e7395536ed325
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-07-10 16:54:15 +0500

    Initial commit

---
+++ src/main/java/kh/farrukh/edvantage/EdVantageApplication.java
@@ -0,0 +1,13 @@
+package kh.farrukh.edvantage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdVantageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdVantageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kh/farrukh/edvantage/EdVantageApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.farrukh.edvantage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdVantageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
