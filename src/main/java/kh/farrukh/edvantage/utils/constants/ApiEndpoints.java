979e2af56eabc080952ba8228646a745bf651cfd
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-07-14 17:24:45 +0500

    Added user controller

--- src/main/java/kh/farrukh/edvantage/utils/constants/ApiEndpoints.java
+++ src/main/java/kh/farrukh/edvantage/utils/constants/ApiEndpoints.java
@@ -5,6 +5,7 @@ public class ApiEndpoints {
     public static final String ENDPOINT_COURSE = "/courses";
     public static final String ENDPOINT_LESSON = "/courses/{courseId}/lessons";
     public static final String ENDPOINT_ROLE = "/roles";
+    public static final String ENDPOINT_USER = "/roles";
 
     public static String withChildEndpoints(String endpoint) {
         return endpoint + "/**";
